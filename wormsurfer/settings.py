cf8ed87a97e92627642e14a6a6f291945d1f38c1
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-23 06:03:18 -0500

    static6

--- wormsurfer/settings.py
+++ wormsurfer/settings.py
@@ -196,9 +196,9 @@ AWS_STORAGE_BUCKET_NAME = 'stonethronepictures'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-STATIC_URL = '/staticfiles'
+STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 django_on_heroku.settings(locals())
