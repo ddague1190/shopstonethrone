392a46e1d0195e6e751cb7e33effe048086f5e9e
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-23 06:25:59 -0500

    static1

--- wormsurfer/settings.py
+++ wormsurfer/settings.py
@@ -19,9 +19,9 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 ALLOWED_HOSTS = ['secure-lowlands-39459.herokuapp.com']
