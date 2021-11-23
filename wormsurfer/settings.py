e8cdd1f485a63e25ee05781c65a4d75905f5bc34
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-23 06:27:52 -0500

    static2

--- wormsurfer/settings.py
+++ wormsurfer/settings.py
@@ -202,10 +202,6 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
