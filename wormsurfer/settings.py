1317d63e8a13c106744db182bb92e46ed8d22c1e
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-23 05:51:10 -0500

    stati4

--- wormsurfer/settings.py
+++ wormsurfer/settings.py
@@ -196,38 +196,9 @@ AWS_STORAGE_BUCKET_NAME = 'stonethronepictures'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_SES_REGION_NAME = 'us-east-1'
-# AWS_SES_REGION_ENDPOINT = 'email.us-east-1.amazonaws.com'
-# AWS_LOCATION = 'static'
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-# STATIC_URL = 'static/'
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'wormsurfer', 'store', 'static')
-# STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles/')
-
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_DIR, 'static/'),
-# )
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 django_on_heroku.settings(locals())
