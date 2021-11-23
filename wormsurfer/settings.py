367f8af20cefde7646af52765f2af181009d6647
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-23 07:17:28 -0500

    5

--- wormsurfer/settings.py
+++ wormsurfer/settings.py
@@ -199,9 +199,9 @@ AWS_STORAGE_BUCKET_NAME = 'stonethronepictures'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
