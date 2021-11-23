d28742788ea6232e7e1a090348487a314ca5e386
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-22 22:18:08 -0500

    Update static_Url

--- wormsurfer/settings.py
+++ wormsurfer/settings.py
@@ -208,13 +208,13 @@ AWS_DEFAULT_ACL = None
 # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'wormsurfer', 'store', 'static')
 # STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles/')
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATICFILES_DIRS = (
 #     os.path.join(PROJECT_DIR, 'static/'),
 # )
