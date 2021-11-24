428fac08dfb093b8136de31a6f279247485f8b6c
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-23 19:39:44 -0500

    5

--- wormsurfer/settings.py
+++ wormsurfer/settings.py
@@ -203,7 +203,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATICFILES_DIRS=[(os.path.join(BASE_DIR,'store/static'))]
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static/store'))]
 
 
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
