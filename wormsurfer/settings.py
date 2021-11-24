3343fe4772e2bdc592e1fff7ae72a94aa3f65e4f
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-23 20:00:21 -0500

    9

--- wormsurfer/settings.py
+++ wormsurfer/settings.py
@@ -1,6 +1,7 @@
 from django.core.handlers import wsgi
 import django_on_heroku
 
+import django_heroku
 import logging
 from pathlib import Path
 import os
@@ -206,7 +207,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGGING = {
     'version': 1,
@@ -223,3 +224,5 @@ LOGGING = {
         },
     },
 }
+
+django_heroku.settings(locals(),staticfiles=False)
