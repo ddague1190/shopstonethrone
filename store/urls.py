f64d7a314223e6d58aac64cd567eb53d27ddfbd7
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-24 05:20:35 -0500

    asdf

--- store/urls.py
+++ store/urls.py
@@ -14,8 +14,13 @@ urlpatterns = [
     path('cart', views.cart, name='cart'),
     path('product/<pk>', views.ProductDetailView.as_view(), name='detailproduct'),
     path('contact', views.contact, name='contact')
-] + static(settings.MEDIA_URL,
-           document_root=settings.MEDIA_ROOT)
+]
 
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-logger = logging.getLogger('django.server')
+    # serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
