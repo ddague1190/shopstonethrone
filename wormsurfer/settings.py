192f85006eae1100be7072c54f3b0cbffc5b2a53
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-24 07:10:07 -0500

    asdf

--- wormsurfer/settings.py
+++ wormsurfer/settings.py
@@ -191,7 +191,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'store/static')
+    os.path.join(BASE_DIR, 'store/static'),
 ]
 
 
