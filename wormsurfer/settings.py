6b4abfc6780661531cc7f0d0d99b1b34199483dc
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-23 19:54:54 -0500

    7

--- wormsurfer/settings.py
+++ wormsurfer/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'sass_processor',
     'store',
     'django_filters',
