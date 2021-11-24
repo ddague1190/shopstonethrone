a93b756de11fc6824f9602b5813853432874c04d
ddague1190 <77030053+ddague1190@users.noreply.github.com>
2021-11-24 06:59:36 -0500

    asdf

--- wormsurfer/settings.py
+++ wormsurfer/settings.py
@@ -190,10 +190,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'store/static')
-
-)
+]
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
